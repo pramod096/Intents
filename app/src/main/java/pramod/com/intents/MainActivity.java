2917f3c9315f4ce754bffbe89f345b38c422bce5
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-07-30 19:59:59 +0530

    Added Text View

--- app/src/main/java/pramod/com/intents/MainActivity.java
+++ app/src/main/java/pramod/com/intents/MainActivity.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.texts);
     }
 
     public void nextActivity(View view) {
+        String text = editText.getText().toString();
         Intent i = new Intent(this, SecondActivity.class);
+        i.putExtra("KEY", text);
         startActivity(i);
+
     }
 }
--- app/src/main/java/pramod/com/intents/SecondActivity.java
+++ app/src/main/java/pramod/com/intents/SecondActivity.java
@@ -3,12 +3,19 @@ package pramod.com.intents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class SecondActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        textView = findViewById(R.id.textView);
+
+        String msg = getIntent().getStringExtra("KEY");
+        textView.setText(msg);
     }
 }
